b35ec8428b98f96a34fa283aa393515c72120511
lurkmorr <lurkmorr@gmail.com>
2022-01-03 18:34:21 +0200

    proper IS_SERVER variable

--- src/const/cfx.ts
+++ src/const/cfx.ts
@@ -1,4 +1,4 @@
 export const CURRENT_RESOURCE: string =
   global.GetCurrentResourceName?.() || 'resource';
 
-export const IS_SERVER: string = global.IsDuplicityVersion?.() || true;
+export const IS_SERVER: string = global.IsDuplicityVersion?.() ?? true;
