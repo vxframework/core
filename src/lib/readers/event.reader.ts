476982739b31684e969ddf7b3ada1c85247e3497
lurkmorr <lurkmorr@gmail.com>
2022-01-06 14:06:14 +0200

    move logic to event reader

--- src/lib/readers/event.reader.ts
+++ src/lib/readers/event.reader.ts
@@ -1,18 +1,50 @@
-import { Injectable, Inject } from '../decorators';
+import { Injectable } from '../decorators';
 import { IMetadataReader } from '../../interfaces';
-import { LocalEventReader } from './local-event.reader';
-import { NetEventReader } from './net-event.reader';
+import { Reflector } from '../reflector';
+import { EventMetadata } from '../../types';
+import { LOCAL_EVENT, NET_EVENT } from '../../const';
+import { Logger } from '../logger';
 
 @Injectable()
 export class EventReader implements IMetadataReader {
-  @Inject(NetEventReader)
-  private netEventReader: NetEventReader;
+  private logger = new Logger('Event');
 
-  @Inject(LocalEventReader)
-  private localEventReader: LocalEventReader;
+  private static log = true;
+
+  public readNet(target: unknown): void {
+    const ctor = target.constructor;
+    const controllerName = Reflector.getControllerName(ctor);
+    const events = Reflector.get<EventMetadata[]>(target, NET_EVENT) || [];
+    Reflect.deleteMetadata(NET_EVENT, target);
+    events.forEach(({ method, event }) => {
+      global.onNet?.(event, target[method].bind(target));
+      if (!EventReader.log) {
+        return null;
+      }
+      this.logger.success(
+        `Net event [${event}] is bound to ${controllerName}.${method}`,
+      );
+    });
+  }
+
+  public readLocal(target: unknown): void {
+    const ctor = target.constructor;
+    const controllerName = Reflector.getControllerName(ctor);
+    const events = Reflector.get<EventMetadata[]>(target, LOCAL_EVENT) || [];
+    Reflect.deleteMetadata(LOCAL_EVENT, target);
+    events.forEach(({ method, event }) => {
+      global.on?.(event, target[method].bind(target));
+      if (!EventReader.log) {
+        return null;
+      }
+      this.logger.success(
+        `Event [${event}] is bound to ${controllerName}.${method}`,
+      );
+    });
+  }
 
   public read(target: unknown): void {
-    this.netEventReader.read(target);
-    this.localEventReader.read(target);
+    this.readNet(target);
+    this.readLocal(target);
   }
 }
